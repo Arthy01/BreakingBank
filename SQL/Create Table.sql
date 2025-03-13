ec8488ea012e373451d4587d1d8fbea1cd774d4e
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2025-03-13 14:23:28 +0100

    Added SQL Table Create Script

---
+++ SQL/Create Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user_data (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE savegames (
+    savegame_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    data JSONB DEFAULT '{}'::jsonb,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user_data(user_id) ON DELETE CASCADE
+);
