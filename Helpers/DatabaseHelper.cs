6ded9a02d3c828aeb25e870c449565a7c82304c7
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2025-03-16 14:18:38 +0100

    Added DatabaseHelper

---
+++ Helpers/DatabaseHelper.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+
+namespace BreakingBank.Helpers
+{
+    public class DatabaseHelper : IDisposable
+    {
+        private readonly NpgsqlDataSource _dataSource;
+        private bool _disposed = false;
+
+        public DatabaseHelper()
+        {
+            // Fest integrierter Verbindungsstring und DataSource-Erstellung
+            var connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=breakingbank";
+            _dataSource = NpgsqlDataSource.Create(connectionString);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing && _dataSource != null)
+                {
+                    _dataSource.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        ~DatabaseHelper()
+        {
+            Dispose(false);
+        }
+    }
+}
