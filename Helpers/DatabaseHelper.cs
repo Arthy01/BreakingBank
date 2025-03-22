a3b94506733ca64c3a0ab53a0358d692930d0ef0
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2025-03-22 17:12:00 +0100

    Added discussed changes
    Credentials in appsettings
    new functionality in DatabaseHelper
    new AuthResponse struct

--- Helpers/DatabaseHelper.cs
+++ Helpers/DatabaseHelper.cs
@@ -1,4 +1,7 @@
 ﻿using Npgsql;
+using BreakingBank.Models;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.Extensions.Configuration;
 
 namespace BreakingBank.Helpers
 {
@@ -7,38 +10,74 @@ namespace BreakingBank.Helpers
         private readonly NpgsqlDataSource _dataSource;
         private bool _disposed = false;
 
-        public DatabaseHelper()
+        public DatabaseHelper(IConfiguration configuration)
         {
-            var connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=breakingbank";
+            var dbCredentials = configuration.GetSection("DBCredentials");
+            var username = dbCredentials["User"];
+            var password = dbCredentials["Password"];
+            var connectionString = $"Host=localhost;Username={username};Password={password};Database=breakingbank";
             _dataSource = NpgsqlDataSource.Create(connectionString);
         }
 
-        public async Task GetAllUsers()
+        public async Task<List<User>> GetAllUsers()
         {
+            var users = new List<User>();
+
             await using var command = _dataSource.CreateCommand("SELECT * FROM user_data");
             await using var reader = await command.ExecuteReaderAsync();
 
             while (await reader.ReadAsync())
             {
-                Console.WriteLine(reader.GetInt32(0));
-                Console.WriteLine(reader.GetString(1));
-                Console.WriteLine(reader.GetString(2));
+                var user = new User(
+                    reader.GetInt32(0), // ID
+                    reader.GetString(1) // Username
+                );
+                users.Add(user);
             }
+
+            return users;
         }
 
-        public async Task GetUserById(int userId)
+        public async Task<User?> GetUserById(User user)
         {
             await using var command = _dataSource.CreateCommand("SELECT * FROM user_data WHERE user_id = @userId");
-            command.Parameters.AddWithValue("userId", userId);
+            command.Parameters.AddWithValue("userId", user.ID);
             await using var reader = await command.ExecuteReaderAsync();
 
-            while (await reader.ReadAsync())
+            if (await reader.ReadAsync())
             {
-                Console.WriteLine(reader.GetInt32(0));
-                Console.WriteLine(reader.GetString(1));
-                Console.WriteLine(reader.GetString(2));
+                return new User(
+                    reader.GetInt32(0), // ID
+                    reader.GetString(1) // Username
+                );
             }
+
+            return null; // When no User is found
         }
+
+        public async Task UpdateUser(User user)
+        {
+            await using var command = _dataSource.CreateCommand("UPDATE user_data SET username = @username WHERE user_id = @userId");
+            command.Parameters.AddWithValue("userId", user.ID);
+            command.Parameters.AddWithValue("username", user.Username);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task UpdateUserPassword(User user, string password)
+        {
+            await using var command = _dataSource.CreateCommand("UPDATE user_data SET password = @password WHERE user_id = @userId");
+            command.Parameters.AddWithValue("userId", user.ID);
+            command.Parameters.AddWithValue("password", password);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task DeleteUser(User user)
+        {
+            await using var command = _dataSource.CreateCommand("DELETE FROM user_data WHERE user_id = @userId");
+            command.Parameters.AddWithValue("userId", user.ID);
+            await command.ExecuteNonQueryAsync();
+        }
+
         public async Task CreateUser(string username, string password)
         {
             await using var command = _dataSource.CreateCommand("INSERT INTO user_data (username, password) VALUES (@username, @password)");
@@ -47,20 +86,25 @@ namespace BreakingBank.Helpers
             await command.ExecuteNonQueryAsync();
         }
 
-        public async Task UpdateUser(int userId, string username, string password)
+        public async Task<AuthResponse> AuthUser(AuthRequest authRequest)
         {
-            await using var command = _dataSource.CreateCommand("UPDATE user_data SET username = @username, password = @password WHERE user_id = @userId");
-            command.Parameters.AddWithValue("userId", userId);
-            command.Parameters.AddWithValue("username", username);
-            command.Parameters.AddWithValue("password", password);
-            await command.ExecuteNonQueryAsync();
-        }
-
-        public async Task DeleteUser(int userId)
-        {
-            await using var command = _dataSource.CreateCommand("DELETE FROM user_data WHERE user_id = @userId");
-            command.Parameters.AddWithValue("userId", userId);
-            await command.ExecuteNonQueryAsync();
+            await using var command = _dataSource.CreateCommand("SELECT user_id FROM user_data WHERE username = @username AND password = @password");
+            command.Parameters.AddWithValue("username", authRequest.Username);
+            command.Parameters.AddWithValue("password", authRequest.Password);
+            await using var reader = await command.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                return new AuthResponse
+                {
+                    Message = "Success",
+                    UserID = reader.GetInt32(0)
+                };
+            }
+            return new AuthResponse
+            {
+                Message = "Failed",
+                UserID = null
+            };
         }
 
         public async Task GetAllSavegames()
---
+++ Models/AuthResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BreakingBank.Models
+{
+    public struct AuthResponse
+    {
+        public string Message { get; set; }
+        public int? UserID { get; set; }
+    }
+}
