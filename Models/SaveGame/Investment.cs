8a6f6b9d2be06eef675e18f663c531eff72920d7
Z4CKHD <133773382+Z4CKHD@users.noreply.github.com>
2025-03-25 13:02:31 +0100

    Create Investment.cs

---
+++ Models/SaveGame/Investment.cs
@@ -0,0 +1,42 @@
+﻿namespace BreakingBank.Models.SaveGame
+{
+    public class Investment : SaveGameData
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public DirtyField<bool> IsPurchased { get; } = new();
+        public ulong Cost { get; }
+        public ulong RevenuePerSecond { get; }
+
+        public Investment(string name, string description, bool isPurchased, ulong cost, ulong revenuePerSecond)
+        {
+            Name = name;
+            Description = description;
+            IsPurchased = new DirtyField<bool>() { Value = isPurchased };
+            Cost = cost;
+            RevenuePerSecond = revenuePerSecond;
+
+            IsPurchased.OnDirtyStateChanged += () => HandleDirtyStateChanged(IsPurchased, nameof(IsPurchased));
+        }
+
+        public override void ClearDirtyData()
+        {
+            IsPurchased.ClearDirty();
+
+            base.ClearDirtyData();
+        }
+
+        public bool CanBuy()
+        {
+            return true;
+        }
+
+        public void Buy()
+        {
+            if (!CanBuy()) return;
+
+            //game.Money -= GetCost();
+            IsPurchased.Value = true;
+        }
+    }
+}
